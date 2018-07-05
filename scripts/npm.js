c86721bf9787e68471a0eb8926c64fde61c6703c
Nguyen The Dai Duong (RBVH/ENG41) <udu1hc@bosch.com>
2018-07-05 15:39:31 +0700

    add async methods

--- scripts/npm.js
+++ scripts/npm.js
@@ -2,55 +2,72 @@
 
 const { spawn, spawnSync } = require('child_process');
 
-function link(module) {
-  if (module) {
-    return _npmLinkModule(module)
-  } else {
-    return _npmLink()
-  }
+const config = require('./config')
+
+function linkSync(module) {
+  const command = ['link'];
+  module && command.push(module);
+  const p = spawnSync('npm', command);
+  config.__verbose && console.log(`${p.stdout}`)
+  console.log(`${p.stderr}`)
+  return p;
 }
 
-function unlink(module) {
-  if (module) {
-    return _npmUnLinkModule(module)
-  } else {
-    return _npmUnLink()
-  }
+function unlinkSync(module) {
+  const command = ['unlink'];
+  module && command.push(module);
+  const p = spawnSync('npm', command);
+  config.__verbose && console.log(`${p.stdout}`)
+  console.log(`${p.stderr}`)
+  return p;
+}
+
+function installSync() {
+  const p = spawnSync('npm', ['install']);
+  config.__verbose && console.log(`${p.stdout}`)
+  console.log(`${p.stderr}`)
+  return p;
 }
 
 function install() {
-  const p = spawnSync('npm', ['install']);
-  console.log(`${p.stdout}`)
-  console.log(`${p.stderr}`)
-  return p;
+  return new Promise((resolve, reject) => {
+    const p = spawn('npm', ['install']);
+    p.on('close', code => {
+      if (code === 0) {
+        resolve(code)
+      } else {
+        reject(code)
+      }
+    })
+
+    if (config.__verbose) {
+      proc.stdout.on('data', (data) => console.log(`${data}`));     
+    }
+    proc.stderr.on('data', (data) => console.log(`${data}`));
+    
+  })
 }
 
-function _npmLink() {
-  const p = spawnSync('npm', ['link']);
-  console.log(`${p.stdout}`)
-  console.log(`${p.stderr}`)
-  return p;
+function link(module) {
+  return new Promise((resolve, reject) => {
+    const command = ['link'];
+    module && command.push(module);
+    const p = spawn('npm', command);
+    p.on('close', code => {
+      if (code === 0) {
+        console.log('\n\nDone linking module: ' + module)
+        resolve(code)
+      } else {
+        reject(code)
+      }
+    })
+
+    if (config.__verbose) {
+      p.stdout.on('data', (data) => console.log(`${data}`));      
+    }
+    p.stderr.on('data', (data) => console.log(`${data}`));
+
+  })
 }
 
-function _npmLinkModule(module) {
-  const p = spawnSync('npm', ['link', module]);
-  console.log(`${p.stdout}`)
-  console.log(`${p.stderr}`)
-  return p;
-}
-
-function _npmUnLink() {
-  const p = spawnSync('npm', ['unlink']);
-  console.log(`${p.stdout}`)
-  console.log(`${p.stderr}`)
-  return p;
-}
-
-function _npmUnLinkModule(module) {
-  const p = spawnSync('npm', ['unlink', module]);
-  console.log(`${p.stdout}`)
-  console.log(`${p.stderr}`)
-  return p;
-}
-
-module.exports = { link, unlink, install }
+module.exports = { link, linkSync, unlinkSync, install, installSync }
